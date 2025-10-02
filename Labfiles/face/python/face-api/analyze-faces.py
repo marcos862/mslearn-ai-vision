e3018d86bf9d63a5a25ae1e20751c008ecd72376
Marcos Bolanos <marcos.ibolanos@gmail.com>
2025-10-02 16:49:32 -0600

    Update analyze-faces.py

--- Labfiles/face/python/face-api/analyze-faces.py
+++ Labfiles/face/python/face-api/analyze-faces.py
@@ -1,14 +1,22 @@
-from dotenv import load_dotenv
 import os
 import sys
+
 from PIL import Image, ImageDraw
+from dotenv import load_dotenv
+from traceback import format_exc
 from matplotlib import pyplot as plt
 
 # Import namespaces
+from azure.ai.vision.face import FaceClient
+from azure.ai.vision.face.models import (
+    FaceDetectionModel,
+    FaceRecognitionModel,
+    FaceAttributeTypeDetection01
+)
+from azure.core.credentials import AzureKeyCredential
 
 
 def main():
-
     # Clear the console
     os.system('cls' if os.name=='nt' else 'clear')
 
@@ -23,20 +31,50 @@ def main():
         if len(sys.argv) > 1:
             image_file = sys.argv[1]
 
-
         # Authenticate Face client
-
-
+        face_client = FaceClient(
+            endpoint=cog_endpoint,
+            credential=AzureKeyCredential(cog_key)
+        )
 
         # Specify facial features to be retrieved
-
+        features = [
+            FaceAttributeTypeDetection01.HEAD_POSE,
+            FaceAttributeTypeDetection01.OCCLUSION,
+            FaceAttributeTypeDetection01.ACCESSORIES
+        ]
 
         # Get faces
- 
- 
+        with open(image_file, mode="rb") as image_data:
+            detected_faces = face_client.detect(
+                image_content=image_data.read(),
+                detection_model=FaceDetectionModel.DETECTION01,
+                recognition_model=FaceRecognitionModel.RECOGNITION01,
+                return_face_id=False,
+                return_face_attributes=features
+            )
 
-    except Exception as ex:
-        print(ex)
+        face_count = 0
+        if len(deteted_faces) > 0:
+            print(len(detected_faces), ' faces detected.')
+
+            for face in detected_faces:
+                face_count += 1
+                print('\nFace number {}'.format(face_count))
+                print(' - Head Pose (Yaw): {}'.format(face.face_attributes.head_pose.yaw))
+                print(' - Head Pose (Pitch): {}'.format(face.face_attributes.head_pose.pitch))
+                print(' - Head Pose (Roll): {}'.format(face.face_attributes.head_pose.roll))
+                print(' - Forehead occluded?: {}'.format(face.face_attributes.occlusion['foreheadOccluded']))
+                print(' - Eye occluded?: {}'.format(face.face_attributes.occlusion['eyeOccluded']))
+                print(' - Mouth occluded?: {}'.format(face.face_attributes.occlusion['mouthOccluded']))
+                print(' - Accessoreies:')
+                for accessory in face.face_attributes.accessories:
+                    print('   - {}'.format(accessory.type))
+                # Annotate faces in the image
+                annotate_faces(image_file, detected_faces)
+
+    except Exception:
+        print(format_exc())
 
 def annotate_faces(image_file, detected_faces):
     print('\nAnnotating faces in image...')
